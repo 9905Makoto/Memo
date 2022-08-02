2f31b42da1af8440740db65e0df5c892af73cc46
9905Makoto <tanukispring1515@gmail.com>
2022-08-02 17:44:47 +0900

    edit views.py

--- memo_pj/memo/views.py
+++ memo_pj/memo/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import Memo
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def index(request):
-    return render(request, 'memo/index.html')
+    memos = Memo.objects.all().order_by('-updated_datetime')
+    return render(request, 'memo/index.html', {'memos': memos})
+
+def detail(request, memo_id):
+    memo = get_object_or_404(Memo, id=memo_id)    
+    return render(request, 'memo/detail.html', {'memo': memo})
+
+def new_memo(request):
+    return render(request, 'memo/new_memo.html')    
