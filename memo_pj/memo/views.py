0bcfa4827f0a8e4657013e39a3f6f82b32891108
9905Makoto <tanukispring1515@gmail.com>
2022-08-02 20:28:08 +0900

    edit views.py

--- memo_pj/memo/views.py
+++ memo_pj/memo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Memo
 from django.shortcuts import get_object_or_404
+from .forms import MemoForm
 
 # Create your views here.
 def index(request):
@@ -13,3 +14,7 @@ def detail(request, memo_id):
 
 def new_memo(request):
     return render(request, 'memo/new_memo.html')    
+
+def new_memo(request):
+    form = MemoForm
+    return render(request, 'memo/new_memo.html', {'form': form})    
